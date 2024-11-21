4e9bd5680abd4ebd84e85958d420ee7d854b2dcf
Antonio Ojeda <ajojeda6@utpl.edu.ec>
2024-11-21 11:21:48 -0500

    if cambios grosos

--- ejemplos-while/EjemploCicloWhile/src/ejemplociclowhile/Ejemplo101.java
+++ ejemplos-while/EjemploCicloWhile/src/ejemplociclowhile/Ejemplo101.java
@@ -20,17 +20,25 @@ public class Ejemplo101 {
         // TODO code application logic here
         Scanner entrada = new Scanner(System.in);
         entrada.useLocale(Locale.US);
-        int limite_tabla = 12;
+        int limite_tabla;
         int contador = 1;
         int operacion;
         int tabla;
         
+        System.out.println("Ingrese el limite");
+        limite_tabla = entrada.nextInt();
         System.out.println("Ingrese el número de tabla a generar");
         tabla = entrada.nextInt();
         
         String cadena = ""; // cadena acumulador
         cadena = String.format("%sTabla de multiplicar\n", cadena);
         
+        if (limite_tabla <=0){
+            limite_tabla = 12;
+        }else{
+                limite_tabla = limite_tabla + 0;
+        }
+                    
         while (contador <= limite_tabla){
             operacion = tabla * contador;
             
